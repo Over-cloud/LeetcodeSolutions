d23e0a4b5b2a2343736bb282d432f415ab289c68
Ruilinzhang0415 <472075013ruilinzhang@gmail.com>
2024-05-06 10:03:48 -0500

    Minor change: add description to remove nodes from linked list

--- solutions/remove_nodes_from_linked_list.h
+++ solutions/remove_nodes_from_linked_list.h
@@ -2,6 +2,13 @@
 
 #include "linked_list.h"
 
+//Description:
+//You are given the head of a linked list.
+//
+//Remove every node which has a node with a greater value anywhere to the right side of it.
+//
+//Return the head of the modified linked list.
+
 namespace RemoveNodesFromLinkedList {
 	using namespace std;
 
